627cce79994e5cfd84bf5d6e7e0e2694123dbe5b
Arjun Praveen Varshney <arjun.varshney1423@gmail.com>
2024-12-22 15:35:30 +0530

    Final push

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,4 +5,13 @@
 @section('title', 'Dashboard')
 
 @section('content')
+    <div class="content-card">
+            <div class="card-header">
+                <h1 style="font-weight: 600; font-size: 20px;">Dashboard</h1>
+            </div>
+            <div style="padding: 20px; display: flex; flex-direction: column; align-items: center; justify-content: center; gap: 20px;">
+                <p style="font-weight: 500; font-size: 16px;">Welcome to the dashboard!</p>
+                <p style="font-weight: 500; font-size: 16px;">Select a module from the left to get started.</p>
+            </div>
+        </div>
 @endsection
